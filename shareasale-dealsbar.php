82166f9ec73c3824da68710b488f25f745f07f33
root <root@localhost.localdomain>
2016-07-15 01:24:04 -0500

    fixed settings shortcut for plugins admin page

--- shareasale-dealsbar.php
+++ shareasale-dealsbar.php
@@ -6,10 +6,11 @@
 	* Author: ShareASale
 	* License: GPLv2 or later
 	*/
- 
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
+
+define( "dealsbar_plugin_filename", plugin_basename( __FILE__ ) );
 
 //require the core plugin class
 require_once plugin_dir_path( __FILE__ ) . 'common/class-shareasale-dealsbar.php';
